637b3fc187b34618ffafadbd3f6730fff4857437
Sunnygupta003 <sunnygupta98004@gmail.com>
2025-11-25 08:36:52 +0530

    Add files via upload

---
+++ Sheet 4/question 1.py
@@ -0,0 +1,6 @@
+m=int(input("enter a number"))
+n=int(input("enter the number"))
+for i in range(0,n):
+    for j in range(0,m):
+        print("*",end=" ")
+    print()
---
+++ Sheet 4/question 9.py
@@ -0,0 +1,5 @@
+n=int(input())
+s=1
+for i in range (n):
+    print("*"*s)
+    s=s+2
